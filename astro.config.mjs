19154e5453ba6ef2452458fbe6833cf9deea9123
Marko Arsić <marko@hypetech.io>
2023-08-25 13:59:46 +0200

    Update domain and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://hypetechdev.github.io",
-    base: "/hypetechedu",
+    site: "https://education.hypetech.io",
     vite: {
         ssr: {
             noExternal: ["@picocss/pico"],
